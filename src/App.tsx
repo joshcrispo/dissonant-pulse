a9e9235edbe29d53fe271e8170d147aab7ad18f0
joshcrispo <c20356061@mytudublin.ie>
2024-09-15 15:23:48 +0100

    changes names

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Events from './pages/Events';
 import About from './pages/About';
-import AdminLogin from './pages/AdminLogin';
+import Login from './pages/Login';
 import Navbar from './components/Navbar';
 
 const App: React.FC = () => {
@@ -15,7 +15,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<Events />} />
         <Route path="/about" element={<About />} />
-        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin" element={<Login />} />
       </Routes>
     </Router>
   );
